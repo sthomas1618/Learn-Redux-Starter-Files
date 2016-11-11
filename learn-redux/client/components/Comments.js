528441c4abcd4b141d942032c5a2e34a77acb951
Stephen Thomas <sthomas1618@gmail.com>
2016-11-11 11:46:30 -0800

    Comments UI

--- learn-redux/client/components/Comments.js
+++ learn-redux/client/components/Comments.js
@@ -1,10 +1,27 @@
 import React from 'react';
 
 const Comments = React.createClass({
+  renderComment(comment, i) {
+    return (
+      <div className="comment" key={i}>
+        <p>
+          <strong>{comment.user}</strong>
+          {comment.text}
+          <button className="remove-comment">&times;</button>
+        </p>
+      </div>
+    );
+  },
+
   render() {
     return (
       <div className="comments">
-        I'm a Comment
+        {this.props.postComments.map(this.renderComment)}
+        <form ref="commentForm" className="comment-form">
+          <input type="text" name="author" placeholder="author" />
+          <input type="text" name="comment" placeholder="comment" />
+          <input type="submit" hidden />
+        </form>
       </div>
     );
   }
--- learn-redux/client/components/Single.js
+++ learn-redux/client/components/Single.js
@@ -4,14 +4,15 @@ import Comments from './Comments';
 
 const Single = React.createClass({
   render() {
-    const { posts, params } = this.props;
+    const { posts, params, comments } = this.props;
     const i = posts.findIndex((post) => post.code === params.postId);
     const post = posts[i];
+    const postComments = comments[params.postId] || [];
 
     return (
       <div className="single-photo">
         <Photo i={i} post={post} {...this.props} />
-        <Comments />
+        <Comments postComments={postComments} />
       </div>
     );
   }
