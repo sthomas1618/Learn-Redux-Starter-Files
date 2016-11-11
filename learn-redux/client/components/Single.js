9603c9095dcb868e1094badee40a67232b3b5da8
Stephen Thomas <sthomas1618@gmail.com>
2016-11-11 11:33:25 -0800

    Implement basic Single photo UI.

--- learn-redux/client/components/Single.js
+++ learn-redux/client/components/Single.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import Photo from './Photo';
+import Comments from './Comments';
 
 const Single = React.createClass({
   render() {
+    const { posts, params } = this.props;
+    const i = posts.findIndex((post) => post.code === params.postId);
+    const post = posts[i];
+
     return (
-        <div className="single-photo">
-          I'm the Single Photo
-        </div>
+      <div className="single-photo">
+        <Photo i={i} post={post} {...this.props} />
+        <Comments />
+      </div>
     );
   }
 });
