6962e4d103d3dc6986421fad6c8c006991f34310
Stephen Thomas <sthomas1618@gmail.com>
2016-11-11 09:23:09 -0800

    Implement Routing

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import css from './styles/style.styl';
-import Main from './components/Main';
 
-render(<Main/>, document.getElementById('root'));
+import Main from './components/Main';
+import Single from './components/Single';
+import PhotoGrid from './components/PhotoGrid';
+
+const route = (
+  <Router history={browserHistory}>
+    <Route path="/" component={Main}>
+      <IndexRoute component={PhotoGrid}></IndexRoute>
+      <Route path="/view/:postId" component={Single}></Route>
+    </Route>
+  </Router>
+);
+
+render(route, document.getElementById('root'));
